b233393e5b6ff7440ae5980da322e9abc2fbc7d6
Yogesh Narendra Tiwari <92584567+yogeshnarendratiwari@users.noreply.github.com>
2021-11-21 15:44:07 +0530

    21-11-2021

--- Matrix/Q.5.cpp
+++ Matrix/Q.5.cpp
@@ -2,57 +2,54 @@
 
 #include <iostream>
 #include <vector>
-#include <map>
 using namespace std;
 
+int MaxRectangle(vector<vector<int>> v, int R, int C)
+{
+    int count1 = 0, index1 = -1;
 
-int MaxRectangle(vector<vector<int>> v , int R,int C){
-    int count1=0,max_count1=0,index1=-1;
-
-    for(int i=0 ; i<R ; i++){
-        for(int j=0 ; j<C ; j++){
-            if(v[i][j]==1)
-            count1++;
+    for (int i = 0; i < R; i++)
+    {
+        for (int j = 0; j < C; j++)
+        {
+            if (v[i][j] == 1)
+                count1++;
         }
-        if(count1>=max_count1){
-            max_count1=count1;
-            index1=i;
+        if (count1 == C)
+        {
+            index1 = i;
         }
-        count1=0;
+        count1 = 0;
     }
 
-    int count2=0,max_count2=0,index2=-1;
+    int count2 = 0, index2 = -1;
 
-    for(int i=0 ; i<R ; i++){
-        for(int j=0 ; j<C ; j++){
-            if(v[i][j]==1 && i!=index1)
-            count2++;
+    for (int i = 0; i < R; i++)
+    {
+        for (int j = 0; j < C; j++)
+        {
+            if (v[i][j] == 1 && i != index1)
+                count2++;
         }
-        if(count2>=max_count2){
-            max_count1=count1;
-            index2=i;
-        }
-        count2=0;
+        if (count2 == C)
+            index2 = i;
+        count2 = 0;
     }
 
-    int area,l=0,b=0;
+    int area, l = 0, b = 0;
 
-    for(int i=index2 ; i<=index1 ; i++){
-        b=b+v[i][0];
-    }
+    b = index1 - index2 + 1;
 
-    for(int i=0 ; i<C ; i++){
-        l=l+v[index2][i];
-    }
+    l = C;
 
-    area=l*b;
+    area = l * b;
     return area;
 }
 
 int main()
 {
-    int R,C;
-    cin>>R>>C;
+    int R, C;
+    cin >> R >> C;
     int ele;
     vector<vector<int>> v;
     vector<int> x;
